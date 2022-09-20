a53bda328181bc6b784fdc702c7423e787085baa
sheridan <you@example.com>
2022-09-20 13:43:58 -0500

    quiz

--- 5_datastore.py
+++ 5_datastore.py
@@ -48,7 +48,7 @@ datastore = { "medical":[
       ]
 }
 
-ourfile = open('retail_space2.csv','w')
+outfile = open('retail_space2.csv','w')
 outfile.write('room-number,use,sq-ft,price\n')
 
 #list=datastore medical
